56ecf3a205ed220c07a0ed204a5b5614ae7882e3
Phodal HUANG <h@phodal.com>
2017-02-18 09:05:40 +0800

    use react native startkit

--- index.android.js
+++ index.android.js
@@ -1,47 +1,12 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+ * Load the App component.
+ *  (All the fun stuff happens in "/src/index.js")
+ *
+ * React Native Starter App
+ * https://github.com/mcnamee/react-native-starter-app
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import AppContainer from './src/';
 
-export default class snowhall extends Component {
-  render() {
-    return (
-      <View>
-        <View style={[styles.actionBar,styles.row]}>
-          <Text style={[styles.title]}>
-            雪堂
-          </Text>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#EE7700',
-  },
-  row:{
-    flexDirection:'row',
-  },
-  title:{
-    color: '#ffffff',
-  },
-  actionBar: {
-    height:49,
-    alignItems:'center',
-    justifyContent: 'center',
-    backgroundColor: '#80d8d8',
-  }
-});
-
-AppRegistry.registerComponent('snowhall', () => snowhall);
+AppRegistry.registerComponent('StarterKit', () => AppContainer);
