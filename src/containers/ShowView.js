4060eeed83a3b3a4b2ba101e85b5f9c669b435d6
Phodal HUANG <h@phodal.com>
2017-02-21 13:05:01 +0800

    [show] make data works

--- src/containers/ShowView.js
+++ src/containers/ShowView.js
@@ -114,44 +114,39 @@ const styles = StyleSheet.create({
   }
 });
 
-let mockData = {
-  "err_code": 0,
-  "err_msg": "success",
-  "data": [
-    {
-      "id": "41",
-      "nickname":"Bob Brown",
-      "avatar":"1",
-      "text": "Behind every successful man there's a lot u unsuccessful years. https://www.google.com/",
-      "original_pic": "",
-      "created_at": "1404709434"
-    },
-    {
-      "id": "40",
-      "nickname":"Jean Kerr",
-      "avatar":"7",
-      "text": "I think success has no rules, but you can learn a lot from failure. ",
-      "original_pic": "",
-      "created_at": "1404708544"
-    },
-    {
-      "id": "39",
-      "nickname":"Colin L. Powell",
-      "avatar":"3",
-      "text": "There are no secrets to success. It is the result of preparation, hard work, and learning from failure. www.youtube.com ",
-      "original_pic": "",
-      "created_at": "1404708455"
-    },
-    {
-      "id": "38",
-      "nickname":"Balzac",
-      "avatar":"5",
-      "text": "There is no such thing as a great talent without great will - power.",
-      "original_pic": "http://lorempixel.com/1000/700/nature/5/",
-      "created_at": "1404707590"
-    }
-  ]
-};
+let mockData = [
+  {
+    "id": "41",
+    "nickname": "Bob Brown",
+    "avatar": "1",
+    "text": "Behind every successful man there's a lot u unsuccessful years. https://www.google.com/",
+    "original_pic": "",
+    "created_at": "1404709434"
+  },
+  {
+    "id": "40",
+    "nickname": "Jean Kerr",
+    "avatar": "7",
+    "text": "I think success has no rules, but you can learn a lot from failure. ",
+    "original_pic": "",
+    "created_at": "1404708544"
+  },
+  {
+    "id": "39",
+    "nickname": "Colin L. Powell",
+    "avatar": "3",
+    "text": "There are no secrets to success. It is the result of preparation, hard work, and learning from failure. www.youtube.com ",
+    "original_pic": "",
+    "created_at": "1404708455"
+  },
+  {
+    "id": "38",
+    "nickname": "Balzac",
+    "avatar": "5",
+    "text": "There is no such thing as a great talent without great will - power.",
+    "original_pic": "http://lorempixel.com/1000/700/nature/5/",
+    "created_at": "1404707590"
+  }];
 
 class ShowView extends Component {
   static componentName = 'ShowView';
@@ -185,13 +180,20 @@ class ShowView extends Component {
 
   _onFetch(page = 1){
     this.setState({
-      timeline: mockData
+      timeline: this.getUpdatedDataSource(mockData)
     });
+  }
+
+  getUpdatedDataSource(data) {
+    let rows = data;
+    let ids = rows.map((obj, index) => index);
+
+    return this.state.timeline.cloneWithRows(rows, ids);
   }
 
   _renderRowView(info) {
     return (
-      <TouchableHighlight underlayColor='transparent' onPress={this._gotoDetails.bind(this, info)}>
+      <View underlayColor='transparent' onPress={this._gotoDetails.bind(this, info)}>
         <View ref={component => this._root = component} style={styles.tweetContainer}>
           <View style={styles.topContainer}>
             <Image source={{uri: info.avatar}} style={styles.avatar} />
@@ -208,26 +210,20 @@ class ShowView extends Component {
             {this._renderMsgImage(info)}
           </View>
           <View style={styles.bottomContainer}>
-            <TouchableHighlight style={styles.bottomTool}>
+            <View style={styles.bottomTool}>
               <Text style={styles.bottomToolText}>评论</Text>
-            </TouchableHighlight>
-            <TouchableHighlight style={styles.bottomTool}>
+            </View>
+            <View style={styles.bottomTool}>
               <Text style={styles.bottomToolText}>赞</Text>
-            </TouchableHighlight>
+            </View>
           </View>
         </View>
-      </TouchableHighlight>
+      </View>
     )
   }
 
   _renderMsgImage(info) {
-    if(info.original_pic) {
-      return (
-        <TouchableHighlight onPress={this._openPhotoBrowser.bind(this, info)}>
-          <Image source={{uri: info.original_pic}} style={[styles.msgImage, { resizeMode: Image.resizeMode.cover }]} />
-        </TouchableHighlight>
-      )
-    }
+
   }
 
   _handleUrlPress(url) {
