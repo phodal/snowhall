945eab055d15772014f46da67c9cb19924eb12d7
Fengda HUANG <h@phodal.com>
2017-02-22 21:24:23 +0800

    [user] create basic user info

--- src/containers/UserCenterView.js
+++ src/containers/UserCenterView.js
@@ -1,19 +1,32 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {
   View,
+  Image,
+  Text,
   ListView,
   ScrollView,
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
 import {Actions} from 'react-native-router-flux';
+import {rootReducer} from '@redux/index';
+import * as UserActions from '@redux/user/actions';
 
 import {AppColors, AppStyles} from '@theme/';
+import {connect} from 'react-redux';
 
 import {
   List,
   ListItem,
 } from '@components/ui/';
+
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = {
+  logout: UserActions.logout,
+};
 
 const styles = StyleSheet.create({
   tabContainer: {
@@ -28,6 +41,18 @@ const styles = StyleSheet.create({
   tabbar_text: {
     color: '#FFF',
   },
+  image: {
+    width: 96,
+    height: 96,
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 10,
+  },
+  name: {
+    flex: 1,
+    alignItems: 'center',
+  }
 });
 
 class UserCenter extends Component {
@@ -35,8 +60,11 @@ class UserCenter extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      user: this.props.user,
+      logout: this.props.logout,
+    }
   }
-
 
   render = () => (
     <View style={styles.tabContainer}>
@@ -44,16 +72,31 @@ class UserCenter extends Component {
         automaticallyAdjustContentInsets={false}
         style={[AppStyles.container]}
       >
-        <ListItem
-          key={`login`}
-          onPress={Actions.login}
-          title='登录'
-          leftIcon={{ name: 'build' }}
-        />
+        { this.state.user && this.state.user.avatar_urls && this.state.user.avatar_urls['96'] ?
+          <Image source={{ uri: this.state.user.avatar_urls['96']}} style={styles.image}/>
+          : null
+        }
+        { this.state.user && this.state.user.name ?
+          <Text style={styles.name}>{this.state.user.name}</Text> : null
+        }
+
+        { this.state.user && this.state.user.name ?
+          <ListItem
+            key={`logout`}
+            onPress={this.state.logout}
+            title='注销'
+            leftIcon={{ name: 'build' }}
+          /> :
+          <ListItem
+            key={`login`}
+            onPress={Actions.login}
+            title='登录'
+            leftIcon={{ name: 'build' }}
+          /> }
+
       </ScrollView>
     </View>
   )
 }
 
-/* Export Component ==================================================================== */
-export default UserCenter;
+export default connect(mapStateToProps, mapDispatchToProps)(UserCenter);
